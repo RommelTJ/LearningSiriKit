25372abec5e8504565386a99c6eff25459c645b8
Rommel Rico <me@rommelrico.com>
2017-11-02 09:01:54 -0700

    BikeTaxiClass enum

--- IntentHandler/IntentHandler.swift
+++ IntentHandler/IntentHandler.swift
@@ -8,6 +8,16 @@
 
 import Intents
 
+enum BikeTaxiClass: String, CustomStringConvertible {
+    case premier
+    case basic
+    
+    // Uppercases the first letter (e.g. .premier becomes Premier)
+    var description: String {
+        return self.rawValue.capitalized
+    }
+}
+
 class IntentHandler: INExtension {
     // TODO - Implement me.
 }
